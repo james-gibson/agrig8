27066c1f56dae9441a4107d16668bde0614dd755
James Gibson <james@aptotude.com>
2015-08-12 19:42:18 -0600

    Include 404 handler

--- app.js
+++ app.js
@@ -27,11 +27,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
-/*app.use(function(req, res, next) {
+app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
-});*/
+});
 
 // error handlers
 
