5663cffacdd724d52d3b3398c91340b4da42d2ad
James Gibson <james@aptotude.com>
2015-08-31 18:22:48 -0600

    Removing dead code and adding semicolon

--- src/handlers/AuthenticationHandler.js
+++ src/handlers/AuthenticationHandler.js
@@ -1,12 +1,10 @@
 /**
  * Created by James on 9/18/14.
  */
-//var fs = require('fs');
-
 exports.authenticate = authenticate;
 
 
 function authenticate(req,res) {
-    res.redirect(307,'/api?token=' + req.token)
+    res.redirect(307,'/api?token=' + req.token);
     //res.send({token: req.token,expires:1418883247, stats:'/stats?token='+req.token});
 }
